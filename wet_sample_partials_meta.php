fb60224374c86d46c562657195be31cced672c67
Robert Wetzlmayr <robert@wetzlmayr.at>
2012-07-05 11:58:39 +0200

    Use $partials interface for latest Txp dev.

--- wet_sample_partials_meta.php
+++ wet_sample_partials_meta.php
@@ -5,6 +5,17 @@ class wet_sample_partials_meta
 	{
 		register_callback(__CLASS__.'_fix', 'article_ui', 'partials_meta');
 	}
+
+	function category_2($rs) {
+		// Render the same markup as core and make some noise about it.
+		return category_popup('Category2', $rs['Category2'], 'category-2').script_js('alert("Woot!")');
+	}
+
+	function title($rs) {
+		if (mb_strlen($rs['Title']) > 5) {
+			return mb_substr($rs['Title'], 0, 5).'&hellip;';
+		}
+	}
 }
 
 /**
@@ -12,15 +23,23 @@ class wet_sample_partials_meta
  */
 function wet_sample_partials_meta_fix($event, $step, &$rs, &$partials)
 {
+	// #1 Create own new partial (hash syntax)
 	// Override the categories dropdown
-	$partials['wet_category_1'] = array('mode' => PARTIAL_VOLATILE, 'selector' => '#category-1');
-	$partials['wet_category_2'] = array('mode' => PARTIAL_VOLATILE, 'selector' => '#category-2');
+	$partials['wet_category_1']['mode'] = PARTIAL_VOLATILE;
+	$partials['wet_category_1']['selector'] = '#category-1';
+	$partials['wet_category_1']['cb'] = 'wet_article_partial_category_1';
+
+	// Create own new partial (array syntax)
+	$partials['wet_category_2'] = array('mode' => PARTIAL_VOLATILE, 'selector' => '#category-2', 'cb' => array('wet_sample_partials_meta', 'category_2'));
+
+	// Modify existing core partial by specifying an own callback
+	$partials['title_value']['cb'] = array('wet_sample_partials_meta', 'title');
 }
 
 /**
  * Fiddle with the article's category selects
  */
-function article_partial_wet_category_1($rs) {
+function wet_article_partial_category_1($rs) {
 	static $cats = null;
 	if ($cats == null) {
 		$cats = getTree('root', 'article');
@@ -36,11 +55,6 @@ function article_partial_wet_category_1($rs) {
 		}
 	}
 	return '<ul class="plain-list"><li>'.join('</li><li>', $out).'</li></ul>';
-}
-
-function article_partial_wet_category_2($rs) {
-	// Render the same markup as core and make some noise about it.
-	return category_popup('Category2', $rs['Category2'], 'category-2').script_js('alert("Woot!")');
 }
 
 if (txpinterface == 'admin') new wet_sample_partials_meta;
